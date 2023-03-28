b2996571bdcb2bee961028e787afcf2235762884
berkantsahin2ag <berkantsahin@2ag.com.tr>
2023-03-28 16:14:01 +0300

    GA Added

--- init_setup.sh
+++ init_setup.sh
@@ -1,5 +1,5 @@
 echo [$(date)]: "START"
-export _VERSION_=3.11.2
+export _VERSION_=3.8
 echo [$(date)]: "creating environment with python ${_VERSION_}"
 conda create --prefix ./env python=${_VERSION_} -y
 echo [$(date)]: "activate environment"
